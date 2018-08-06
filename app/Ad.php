9f9d509c79a7c32a570f9edcb927d1fc1faa9f01
Daniel Schmelz <daniel.schmelz@arvato.com>
2018-08-06 13:19:12 +0200

    Add a config for travis

--- app/Ad.php
+++ app/Ad.php
@@ -3,9 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\ModelStatus\HasStatuses;
 
 class Ad extends Model
 {
-    use HasStatuses;
+    use HasStatuses, LogsActivity;
+
+    protected static $logAttributes = ['id'];
+    protected static $logName = 'content';
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function channels()
+    {
+        return $this->belongsToMany(AdChannel::class);
+    }
 }
