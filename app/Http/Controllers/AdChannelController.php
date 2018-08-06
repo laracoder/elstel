8329cf1ff753487b79ab31118ef7e0b649b4cb4b
Daniel Schmelz <daniel.schmelz@arvato.com>
2018-08-06 13:20:15 +0200

    add a dedicated dockerfile for development

---
+++ app/Http/Controllers/AdChannelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AdChannel;
+use Illuminate\Http\Request;
+
+class AdChannelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AdChannel  $adChannel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdChannel $adChannel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AdChannel  $adChannel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdChannel $adChannel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AdChannel  $adChannel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdChannel $adChannel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AdChannel  $adChannel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdChannel $adChannel)
+    {
+        //
+    }
+}
